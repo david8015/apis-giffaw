003780b0911a1d6e1f9b19d8a2493c71b3ddea57
David Hernandez <david.hernandez8015@gmail.com>
2019-09-24 11:26:31 -0400

    initial commit

--- starter-code/scripts/app.js
+++ starter-code/scripts/app.js
@@ -1,7 +1,66 @@
-$(document).on("ready", function(){
 
+(function loadInitialContent(){
+    fetch('https://api.giphy.com/v1/gifs/trending?api_key=8JIWan2gomImYIS6z7DKsEcRbV7UN6u3&limit=25&rating=G', {
+    })
+
+    .then(function(response) {
+        return response.json()
+    })
+    .then(onSuccess)
+    .catch(onError);
+    
+    function onSuccess(response) {
+       let e = document.querySelector(".gif-gallery");
+
+    for (let i=0; i < response.data.length; i++){
+       let elem = document.createElement("img");
+       elem.setAttribute("src", response.data[i].images.fixed_height.url);
+
+       e.appendChild(elem);
+       }
+    }  
+    function onError(error) {
+      alert("Sorry, there was a problem!");
+      console.dir(error);
+    }
+})();
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelector(".btn-primary").addEventListener('click', function(e){
+    e.preventDefault();
+    let baseUrl = "https://api.giphy.com/v1/gifs";
+    let search = document.querySelector(".gif-input").value;
+    let key = "8JIWan2gomImYIS6z7DKsEcRbV7UN6u3";
+    let responses = 25;
+    
+    let fullUrl = `${baseUrl}/search?api_key=${key}&q=${search}&limit=${responses}&offset=0&rating=G&lang=en`;
+    
+    fetch(fullUrl, {
+    })
+
+    .then(function(response) {
+        return response.json()
+    })
+    .then(onSuccess)
+    .catch(onError);
+    
+    function onSuccess(response) {
+       let e = document.querySelector(".gif-gallery");
+
+      //  clear the gif gallery
+       e.innerHTML = "";
+
+    for (let i=0; i < response.data.length; i++){
+       let elem = document.createElement("img");
+       elem.setAttribute("src", response.data[i].images.fixed_height.url);
+
+       e.appendChild(elem);
+       }
+    }  
+    function onError(error) {
+      alert("Sorry, there was a problem!");
+      console.dir(error);
+    }
+  });
 });
-
-// 8JIWan2gomImYIS6z7DKsEcRbV7UN6u3
-
-// http://api.giphy.com/v1/gifs/search?q=cats&api_key=dc6zaTOxFJmzC
